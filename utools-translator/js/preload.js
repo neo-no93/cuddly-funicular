b1355bda04a39b1d59187b0dd84682e4f5ab5643
neono93 <zjybjtu@gmail.com>
2022-10-03 01:51:29 +0800

    添加百度翻译

--- utools-translator/js/preload.js
+++ utools-translator/js/preload.js
@@ -1,7 +1,3 @@
-utools.ubrowser.goto('https://cn.bing.com')
-.value('#sb_form_q', 'uTools')
-.click('#sb_form_go')
-.run({ width: 1000, height: 600 })
 
 window.exports = {
     "trans": { // 注意：键对应的是 plugin.json 中的 features.code
@@ -10,13 +6,22 @@ window.exports = {
           // 进入插件应用时调用（可选）
           enter: (action, callbackSetList) => {
              // 如果进入插件应用就要显示列表数据
-             callbackSetList([
+             callbackSetList(
+               [
                    {
-                      title: '这是标题',
-                      description: '这是描述',
-                      icon:'' // 图标(可选)
-                   }
-             ])
+                      title: '微软翻译',
+                      description: 'bing',
+                      icon:'', // 图标(可选)
+                      url: "https://cn.bing.com/translator"
+                   },
+                   {
+                     title: '百度翻译',
+                     description: 'baidu',
+                     icon:'', // 图标(可选)
+                     url: "https://fanyi.baidu.com/"
+                  }
+             ]
+             )
           },
           // 子输入框内容变化时被调用 可选 (未设置则无搜索)
           search: (action, searchWord, callbackSetList) => {
@@ -27,7 +32,7 @@ window.exports = {
                    title: '微软翻译',
                    description: 'bing',
                    icon:'', // 图标
-                   url: 'https://cn.bing.com/translator?ref=TThis&text=&from=en&to=zh-Hans'
+                   url: 'https://cn.bing.com/translator'
                 }
              ])
           },
@@ -35,7 +40,8 @@ window.exports = {
           select: (action, itemData, callbackSetList) => {
              window.utools.hideMainWindow()
              const url = itemData.url
-             require('electron').shell.openExternal(url)
+             window.utools.ubrowser.goto(url)
+             .run({ width: 1000, height: 600 })
              window.utools.outPlugin()
           },
           // 子输入框为空时的占位符，默认为字符串"搜索"
